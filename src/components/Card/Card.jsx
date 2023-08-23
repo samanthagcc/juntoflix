c4dd9afff46ed334a1e7371c39e7e4bd56be49f9
Samantha Cardoso <samanthagc@juntoseguros.com>
2023-08-23 14:45:33 -0300

    feature/card-image

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,10 @@ export function Card({ props }) {
 	return (
 		<div className={styles['card-container']}>
 			<div className={styles['card-image']}>
-				<img src='' alt='capa da HQ' />
+				<img
+					src='https://baudashqs.com/wp-content/uploads/2022/12/20221223_192632.jpg'
+					alt='capa da HQ'
+				/>
 			</div>
 			<div className={styles['card-title']}>
 				<h2>{props.title}</h2>
