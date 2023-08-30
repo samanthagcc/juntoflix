9be710840e12ed9883609aa92042bb8e2ca46467
Samantha Cardoso <samanthagc@juntoseguros.com>
2023-08-30 09:21:09 -0300

    feat/search-component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
+import Search from '../Search';
 import styles from './styles.module.css';
 
-export function Header() {
+export default function Header() {
 	return (
 		<header className={styles.header}>
 			<div className={styles.wrapper}>
 				<h1> JuntoFlix</h1>
-				<input type='' placeholder='Pesquise aqui' />
+				<Search />
 			</div>
 		</header>
 	);
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,5 @@
+import './styles.module.css';
+
+export default function Search() {
+	return <input type='search' placeholder='Pesquise aqui' />;
+}
